9d27dda6a6a58d3f85a6ab25053d69f76f3b6895
Poshan Pandey <poshan0126@gmail.com>
2019-06-20 08:24:53 +0545

    dart oop basic learned
    learned the basics of oop using the dart language

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,116 @@
+/*//Basics of Dart programming
+
+var myOutsideVariable = 'Ram';
+void main(){
+ /* print('Hello Poshan!');
+  var country = "Nepal";
+  print('My country name is '+ country);//single quote is also used mostly
+  String name;
+  name = 'Poshan';
+  print(name);
+  num a = 4;//you can also use int or double
+  num b = 5;
+  print(a+b);
+  bool isTrue = true;
+  print(isTrue);
+  const pi = 3.14;//you can also use final
+  print(pi);
+  const pi = 3.14;
+  print('The value of pi is $pi');
+  print('The value of pi is ${pi.toInt()}');
+  print(4%3);
+  int a = 67;
+  double b = 34.678;
+  print(a>=b);//>=, <=, !=, ==, are comparisions.
+  print(a is double);//is,is! are also comparisions
+  if(a is int){
+    print('Poshan is learning.');
+  }
+  else{
+    print('no matter.');
+  }
+  if(!(a is int)&&(b is double)){
+    print('Poshan is learning.');// !, ||, @@ are logical operators.
+  }
+  else{
+    print('no matter.');
+  }
+  for(int i=0; i<5;i++)
+  {
+    print('Turn:$i');
+  }*/
+  /*String name = 'Poshan';//contains works
+  if(name.contains('P')){
+    print('Hurray!');
+  }
+  doSomething();
+  print('${function2()}');
+  */
+  var name = sayHello('poshan');
+ print(sayHello('Poshan'));
+ print(name);
+}/*
+doSomething(){
+  print('Function learnt');
+}
+String function2() =>"James bond";//=>means return*/
+sayHello(String name){
+  return "Hello $name";
+}
+//if we are using multiple p;aramaters and  we want some to be optional then we use paramater inside []
+/*if you want to use variable all over you can define variable outside the 
+main function*/
+
+
+ */
+//Object oriented practice
 class House{
   //Instance variables or member variable
-  String name = 'Pandey Niwash';
+  String name; //= 'Pandey Niwash';
   String color;
+  
   int storey;
+  //setter and getter
+  String get getName => name;
+  set setName(String value)=> name= value;
+  /*
+  //constructor
+  House(name,color,storey){
+    this.name=name;
+    this.color= color;
+    this.storey= storey;
+  }
+  */
+//syntatic sugar constructor
+House(this.name,this.color,this.storey);
+//Named constructor(gives specific value to constructors)
+House.spec(){
+  name='Nehou';
+  color='Red';
+  storey = 5;
+}
+
+  void displayInfo(){
+    print('Name: $name Color: $color Storey: $storey' );
+  }
+  bool isAvailable()=>true;
+  int getPrice()=>25000000;
 
 }
 
 main(){
-  var house1 = new House();//creating object.
-  house1.color= 'blue';
+  var secondHouse = new House.spec();
+  var house1 = new House('Pandey Niwash','blue',5);//creating object.
+house1.setName='Poshan';
+print(house1.getName) ;
+ //house1.color= 'blue';
   //house1.name = ;
-  house1.storey = 5 ;
+ // house1.storey = 5 ;
 print(house1);//prints 'Instance of House'
 print('The name of that ${house1.storey} storey and ${house1.color} colored house is ${house1.name} .');
-
+house1.displayInfo();
+print(house1.getPrice());
+print(house1.isAvailable());
+secondHouse.displayInfo();
 
 }
